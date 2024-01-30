53a8e4b52575660828dc5ce219991ef7bb0e9760
EVERLYNE AKINYI <evahaudi2354@gmail.com>
2024-01-30 19:31:20 +0300

    created user model

---
+++ core/users/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Users, Freelancer, Client
+# Register your models here.
+
+admin.site.register(Users)
+admin.site.register(Freelancer)
+admin.site.register(Client)
